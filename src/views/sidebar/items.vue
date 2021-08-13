54c761d3548ddb6b398c965257e373001b8c9668
raykira <thebloodismywater@gmail.com>
2021-08-13 18:18:25 +0300

    axiios

--- src/views/sidebar/items.vue
+++ src/views/sidebar/items.vue
@@ -2,64 +2,188 @@
   <div>
     <v-data-table
       :headers="headers"
-      :items="HangingOrders"
+      :items="items"
       :items-per-page="5"
       class="elevation-0"
     >
-      <template v-slot:item.action="{}">
-        <v-dialog transition="dialog-bottom-transition">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="#7E57C2" dark v-bind="attrs" v-on="on">details</v-btn>
-          </template>
-          <template>
-            <v-card>
-              <v-card-text  style="padding: 20px 0 0 0">
-                <add-item />
-              </v-card-text>
-              <v-card-actions class="justify-end">
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-dialog>
+      <template v-slot:item.action="{item}">
+        <v-btn color="primary" dark @click="openDetails(item)"> Details </v-btn>
       </template>
     </v-data-table>
+
     <template>
-        <v-btn
-          color="green"
-          dark
-          @click.stop="dialog = true"
-        >
-          Add New Item
-        </v-btn>
-
-        <v-dialog
-          v-model="dialog"
-        >
+      <v-dialog v-model="dialog" max-width="800">
+        <template>
           <v-card>
-            <v-card-title class="text-h5">
-            </v-card-title>
-
             <v-card-text>
-              <add-item />
+              <div class="addnewitem">
+                <div
+                  class="ml-auto d-flex"
+                  width="800px"
+                  style="justify-content: center; padding: 0"
+                >
+                  <v-card class="" max-width="" width="" style="maring: 0">
+                    <!-- <v-card-subtitle
+          class="pb-0"
+          style="background-color: #7e57c2; color: white"
+        >
+          Item
+        </v-card-subtitle> -->
+                    <v-container>
+                      <v-row>
+                        <v-col cols="6">
+                          <v-card-text class="text--primary">
+                            <v-row>
+                              <div>
+                                <v-text-field
+                                  label="Item ID"
+                                  :rules="rules"
+                                  hide-details="auto"
+                                  style="width: 300px"
+                                  v-model="itemSelected.id"
+                                ></v-text-field>
+                              </div>
+                            </v-row>
+                            <v-row>
+                              <div>
+                                <v-text-field
+                                  style="width: 300px"
+                                  label="Item Name"
+                                  v-model="itemSelected.name"
+                                ></v-text-field>
+                              </div>
+                            </v-row>
+                            <v-row>
+                              <div>
+                                <v-text-field
+                                  style="width: 300px"
+                                  label="Description"
+                                  v-model="itemSelected.description"
+                                ></v-text-field>
+                              </div>
+                            </v-row>
+                            <v-row>
+                              <div>
+                                <v-text-field
+                                  style="width: 300px"
+                                  label="Price"
+                                  v-model="itemSelected.price"
+                                ></v-text-field>
+                              </div>
+                            </v-row>
+                            <v-row>
+                              <div>
+                                <v-text-field
+                                  style="width: 300px"
+                                  label="Photo"
+                                  
+                                ></v-text-field>
+                              </div>
+                            </v-row>
+                            <v-row>
+                              <div>
+                                <v-text-field
+                                  style="width: 300px"
+                                  label="Color"
+                                  v-model="itemSelected.color"
+                                ></v-text-field>
+                              </div>
+                            </v-row>
+                          </v-card-text>
+                        </v-col>
+                        <v-col cols="6">
+                          <v-row>
+                            <v-img
+                              class="white--text align-end"
+                              height="200"
+                              width="300"
+                              src="../../assets/phone.jpg"
+                            >
+                              <v-card-title>Black Case</v-card-title>
+                            </v-img>
+                          </v-row>
+                          <v-row>
+                            <v-btn
+                              color="#ea4642"
+                              class="ma-3 white--text"
+                            >
+                              Upload Image
+                              <v-icon right dark> mdi-cloud-upload </v-icon>
+                            </v-btn>
+                          </v-row>
+                          <v-row>
+                            <v-col cols="6">
+                              <div>
+                                <v-text-field label="Length" v-model="itemSelected.length"></v-text-field>
+                              </div>
+                            </v-col>
+                            <v-col cols="6">
+                              <div>
+                                <v-text-field label="Weight" v-model="itemSelected.weight"></v-text-field>
+                              </div>
+                            </v-col>
+                          </v-row>
+                          <v-row>
+                            <v-col cols="6">
+                              <div>
+                                <v-text-field label="Height" v-model="itemSelected.height"></v-text-field>
+                              </div>
+                            </v-col>
+                            <v-col cols="6">
+                              <div>
+                                <v-text-field label="Width" v-model="itemSelected.width"></v-text-field>
+                              </div>
+                            </v-col>
+                          </v-row>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+
+                    <v-card-actions>
+                      <v-checkbox
+                        label="Out Of Stock"
+                        color="red"
+                        value="red"
+                        hide-details
+                        style="padding-bottom: 30px"
+                      ></v-checkbox>
+                      <v-text-field style="padding: 0 35px" label="barcode">
+                      </v-text-field>
+                      <v-btn color="#fc9107" text class="ml-auto"> Add </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </div>
+              </div>
             </v-card-text>
 
-            <v-card-actions>
-             
-            </v-card-actions>
+            <v-card-actions> </v-card-actions>
           </v-card>
-        </v-dialog>
+        </template>
+      </v-dialog>
+    </template>
+
+    <template>
+      <v-btn color="green" dark @click.stop="dialog = true">
+        Add New Item
+      </v-btn>
     </template>
   </div>
 </template>
 
 <script>
-import AddItem from '../../components/add-item.vue';
+// import AddItem from "../../components/add-item.vue";
+import axios from "axios";
+
 export default {
-  components: { AddItem },
+  // components: { AddItem },
   name: "Bills",
   data() {
     return {
       dialog: false,
+      rules: [
+        (value) => !!value || "Required.",
+        (value) => (value && value.length >= 3) || "Min 3 characters",
+      ],
       headers: [
         {
           text: "Item Id",
@@ -70,49 +194,44 @@ export default {
         { text: "Item Name", value: "name" },
         { text: "Details", value: "action" },
       ],
-      HangingOrders: [
-        {
-          name: "Car",
-          id: 159,
-        },
-        {
-          name: "Chair",
-          id: 237,
-        },
-        {
-          name: "Door",
-          id: 262,
-        },
-        {
-          name: "Table",
-          id: 305,
-        },
-        {
-          name: "Book",
-          id: 356,
-        },
-        {
-          name: "Knife",
-          id: 375,
-        },
-        {
-          name: "Fork",
-          id: 392,
-        },
-        {
-          name: "Spoon",
-          id: 408,
-        },
-        {
-          name: "Plate",
-          id: 452,
-        },
-        {
-          name: "House",
-          id: 518,
-        },
-      ],
+      items: [],
+      itemSelected: {
+        "id": null,
+        "name": "",
+        "description": "",
+        "price": "",
+        "photo": null,
+        "color": "",
+        "outOfStock": "",
+        "length": "",
+        "width": "",
+        "height": "",
+        "weight": "",
+        "Barcode": "",
+      },
     };
+  },
+  created() {
+    this.fetch();
+  },
+  methods: {
+    fetch() {
+      axios
+        .get(`https://backend.argussys.site/api/items`)
+        .then((response) => {
+          console.log(response.data);
+          this.items = response.data.data;
+        })
+        .catch((error) => {
+          console.log("Error: ", error.response.data);
+        });
+    },
+    openDetails(item) {
+      console.log("Cilcked....");
+      this.dialog = true;
+      console.log(item);  
+      this.itemSelected = item;
+    },
   },
 };
 </script>
--- src/views/sidebar/warehouse-balance.vue
+++ src/views/sidebar/warehouse-balance.vue
@@ -13,15 +13,111 @@
         single-line
       ></v-select>
     </v-row>
-    <warehouse-table />
+    <template>
+      <v-data-table
+        :headers="headers"
+        :items="warehouses"
+        sort-by="calories"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>My CRUD</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  class="mb-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  New Item
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          disabled
+                          v-model="editedItem.id"
+                          label="Warehouse name"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.name"
+                          label="Item Name"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.address"
+                          label="Address"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.phone"
+                          label="Phone"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">
+                    Cancel
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="text-h5"
+                  >Are you sure you want to delete this warehouse?</v-card-title
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete"
+                    >Cancel</v-btn
+                  >
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                    >OK</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template>
+      </v-data-table>
+    </template>
   </div>
 </template>
 <script>
-import WarehouseTable from "../../components/warehouse-balance/warehouse-table";
+import axios from "axios";
 export default {
-  components: {
-    WarehouseTable,
-  },
   data() {
     return {
       select: { warehouse: "All", abbr: "All" },
@@ -32,7 +128,161 @@ export default {
         { warehouse: "California Warehouse", abbr: "CA" },
         { warehouse: "New York Warehouse", abbr: "NY" },
       ],
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: "Warehouse Id",
+          align: "start",
+          filterable: false,
+          value: "id",
+        },
+        { text: "Warehouse Name", value: "name" },
+        // { text: "Description", value: "description" },
+        // { text: "Line Number", value: "line" },
+        // { text: "Amount", value: "amount" },
+        // { text: "Price", value: "price" },
+        { text: "Actions", value: "actions", sortable: false },
+      ],
+      warehouses: [],
+      warehouseCurrent: {
+        id: null,
+        name: "",
+        address: "",
+        phone: "",
+      },
+      editedIndex: -1,
+      editedItem: {
+        id: null,
+        name: "",
+        address: "",
+        phone: "",
+      },
+      defaultItem: {
+        id: null,
+        name: "",
+        address: "",
+        phone: "",
+      },
     };
+  },
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edite Warehouse";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+    this.fetch();
+  },
+
+  methods: {
+    initialize() {
+      this.warehouses = [
+        {
+          // desctiption: "description here",
+          // line: 24,
+          // amount: 4.0,
+          // price: "1$",
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.warehouses.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.warehouses.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.warehouses.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      var warehouse = this.editedItem;
+      if (this.editedIndex > -1) {
+        // Update
+
+        axios
+          .put(`https://backend.argussys.site/api/warehouses/${warehouse.id}`, {
+            name: warehouse.name,
+            address: warehouse.address,
+            phone: warehouse.phone,
+          })
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log("Error: ", error);
+          });
+        Object.assign(this.warehouses[this.editedIndex], this.editedItem);
+      } else {
+        // ADD
+
+        axios
+          .post(`https://backend.argussys.site/api/warehouses`, {
+            name: warehouse.name,
+            address: warehouse.address,
+            phone: warehouse.phone,
+          })
+          .then((response) => {
+            console.log(response.data);
+            this.editItem = response.data.data;
+            this.warehouses.push(this.editedItem);
+            location.reload();
+          })
+          .catch((error) => {
+            console.log("Error: ", error);
+          });
+      }
+      this.close();
+      
+    },
+    fetch() {
+      axios
+        .get("https://backend.argussys.site/api/warehouses")
+        .then((response) => {
+          console.log(response.data);
+          this.warehouses = response.data.data;
+        })
+        .catch((error) => {
+          console.log("Error: ", error.response.data);
+        });
+    },
   },
 };
 </script>
